64faecc9648e4d01b7cf60ae37738e75d587d778
Daniel Boadzie <boadziedaniel43@gmail.com>
2020-08-03 19:32:04 +0000

    added user actions, toggle action and nested resolver

--- src/schema.js
+++ src/schema.js
@@ -6,7 +6,9 @@ module.exports = gql`
   type Note {
     id: ID!
     content: String!
-    author: String!
+    author: User!
+    favoriteCount: Int!
+    favoritedBy: [User]
     createdAt: DateTime!
     updatedAt: DateTime!
   }
@@ -17,17 +19,27 @@ module.exports = gql`
     email: String!
     avatar: String
     notes: [Note!]!
+    favorites: [Note!]!
   }
 
+  type NoteFeed {
+    notes: [Note]!
+    cursor: String!
+    hasNextPage: Boolean!
+  }
   type Query {
     notes: [Note!]!
-    note(id: ID!): Note!
+    note(id: ID): Note!
+    user(username: String!): User
+    users: [User!]!
+    me: User!
+    # noteFeed(cursor: String): NoteFeed
   }
-
   type Mutation {
-    newNote(content: String!): Note!
+    newNote(content: String!): Note
     updateNote(id: ID!, content: String!): Note!
     deleteNote(id: ID!): Boolean!
+    toggleFavorite(id: ID!): Note!
     signUp(username: String!, email: String!, password: String!): String!
     signIn(username: String, email: String, password: String!): String!
   }
